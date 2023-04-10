1b1d10bf7a0b1a8bf519c664ec53074e19d37e3b
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-04-10 14:54:44 -0700

    Removed HW3 comments

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -8,9 +8,6 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-    /// <summary>
-    /// Connor Hodges
-    /// </summary>
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
