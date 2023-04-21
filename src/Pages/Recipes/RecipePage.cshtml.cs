49307676c14a028725710c77395c878c1237fbda
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-04-21 15:18:47 -0700

    Modified RecipePage.cshtml.cs to intialize JsonFileRecipeService in its constructor

--- src/Pages/Recipes/RecipePage.cshtml.cs
+++ src/Pages/Recipes/RecipePage.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +11,13 @@ namespace ContosoCrafts.WebSite.Pages.Recipes
 {
 	public class RecipePageModel : PageModel
     {
+        public RecipePageModel(JsonFileRecipeService recipeService)
+        {
+            RecipeService = recipeService;
+        }
+
+        public JsonFileRecipeService RecipeService { get; set; }
+
         public void OnGet()
         {
         }
