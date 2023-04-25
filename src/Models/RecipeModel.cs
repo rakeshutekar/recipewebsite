04b1f8d9c6e7f1be3cfc950ece94f2f00899e8ba
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-04-24 18:23:36 -0700

    Modified RecipeModel.cs to include class comments

--- src/Models/RecipeModel.cs
+++ src/Models/RecipeModel.cs
@@ -3,15 +3,27 @@ using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// RecipeModel class is used to represent recipe JSON data in object form
+    /// </summary>
     public class RecipeModel
     {
+        /*PRIVATE FIELDS*/
+        // Default description when the description field is empty
         private string description = "Default Description";
 
+        /*PUBLIC VARIABLES*/
+        // ID of the recipe from the JSON file
         public int RecipeID{get;set;}
+        // ID of the author from the JSON file
         public int AuthorID{get;set;}
+        // First name of the writer
         public string FirstName{get;set;}
+        // Last name of the writer
         public string LastName{get;set;}
+        // Title of the recipe
         public string Title{get;set;}
+        // Short description of the recipe
         public string Description
         {
             get => description;
@@ -21,14 +33,21 @@ namespace ContosoCrafts.WebSite.Models
                 description = value;
             }
         }
+        // Array of instructions for the recipe
         public string[] Instructions{get;set;}
+        // Array of ingredients for the recipe
         public string[] Ingredients{get;set;}
+        // Date of publishing the recipe on the webiste
         public string PublishDate{get;set;}
+        // Date of last edit of the recipe 
         public string EditDate{get;set;}
+        // Tags associated with the recipe, used for filtering
         public string[] Tags{get;set;}
+        // Image URL of imgae associated with the recipe
         [JsonPropertyName("Img")]
         public string Image{get;set;}
 
+        // Overridden ToString method to serialize the object's data
         public override string ToString() => JsonSerializer.Serialize<RecipeModel>(this);
  
     }
