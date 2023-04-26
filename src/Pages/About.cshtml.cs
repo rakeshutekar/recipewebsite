71d4e5b4b769c9a630f8a3e680d48a13248c42c2
twbernhard <torbenwbernhard@gmail.com>
2023-04-26 15:39:33 -0700

    Update About.cshtml.cs
    Added comment documentation

--- src/Pages/About.cshtml.cs
+++ src/Pages/About.cshtml.cs
@@ -3,15 +3,25 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Represents a model for the About page that provides information about the application.
+    /// </summary>
     public class About : PageModel
     {
         private readonly ILogger<About> _logger;
 
+        /// <summary>
+        /// Initializes a new instance of the About class with the specified logger.
+        /// </summary>
+        /// <param name="logger">The logger to use for logging information about the application.</param>
         public About(ILogger<About> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Handles HTTP GET requests for the About page.
+        /// </summary>
         public void OnGet()
         {
         }
