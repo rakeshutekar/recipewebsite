cdc406cbe3e3d835aeb8d1f0bef83a3ba5d75d53
twbernhard <torbenwbernhard@gmail.com>
2023-04-26 15:38:27 -0700

    Update Privacy.cshtml.cs
    Added comments to Privacy page

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,15 +3,25 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Represents a model for the Privacy page that provides privacy policy information.
+    /// </summary>
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Initializes a new instance of the PrivacyModel class with the specified logger.
+        /// </summary>
+        /// <param name="logger">The logger to use for logging privacy-related information.</param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Handles HTTP GET requests for the Privacy page.
+        /// </summary>
         public void OnGet()
         {
         }
