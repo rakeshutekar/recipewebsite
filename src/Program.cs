6d92d8ab1f78e29b944e84a65636763ae8f70dfc
twbernhard <torbenwbernhard@gmail.com>
2023-04-26 15:26:04 -0700

    Update Program.cs
    Added comments to Program file

--- src/Program.cs
+++ src/Program.cs
@@ -6,11 +6,17 @@ namespace ContosoCrafts.WebSite
 {
     public class Program
     {
+        /// <summary>
+        /// Entry point in to program.
+        /// </summary>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Create host builder using Startup class.
+        /// </summary>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
