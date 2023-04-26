37222e0f795217c6876cb1e34c4bd1e8bec2d069
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-04-26 13:10:23 -0700

    Modified /Recipes/Index.cshtml.cs to remove ference to the ProductModel/Service

--- src/Pages/Recipes/Index.cshtml.cs
+++ src/Pages/Recipes/Index.cshtml.cs
@@ -20,14 +20,11 @@ namespace ContosoCrafts.WebSite.Pages.Recipes
             RecipeService = recipeService;
         }
 
-        public JsonFileProductService ProductService { get; }
         public JsonFileRecipeService RecipeService{get;}
-        public IEnumerable<ProductModel> Products { get; private set; }
         public IEnumerable<RecipeModel> Recipes {get; private set;}
 
         public void OnGet()
         {
-            // Products = ProductService.GetProducts();
             Recipes = RecipeService.GetRecipes();
         }
     }
