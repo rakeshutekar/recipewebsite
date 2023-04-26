a18e9fc9b11e1d21a9df7d033e2ab37f62c9e2ed
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-04-26 16:55:24 -0700

    Modified /Recipes/Index.cshtml.cs to include class comments

--- src/Pages/Recipes/Index.cshtml.cs
+++ src/Pages/Recipes/Index.cshtml.cs
@@ -9,10 +9,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoCrafts.WebSite.Pages.Recipes
 {
+    /// <summary>
+    /// IndexList is the c# model behind the index page of the recipes. This
+    /// class retrieves the recipes and shows the user a full listing of the
+    /// catalogue.
+    /// </summary>
     public class IndexList : PageModel
     {
+        // Logger field
         private readonly ILogger<IndexModel> _logger;
 
+        /// <summary>
+        /// Default constructor that injects the logger and recipeService via DI
+        /// for use with the page
+        /// </summary>
+        /// <param name="logger">Logger</param>
+        /// <param name="recipeService">RecipeService that retrieves data</param>
         public IndexList(ILogger<IndexModel> logger,
             JsonFileRecipeService recipeService)
         {
@@ -20,9 +32,15 @@ namespace ContosoCrafts.WebSite.Pages.Recipes
             RecipeService = recipeService;
         }
 
+        // Service that retrieves the data
         public JsonFileRecipeService RecipeService{get;}
+        // Collection of recipes retrieved from the database
         public IEnumerable<RecipeModel> Recipes {get; private set;}
 
+        /// <summary>
+        /// OnGet() method retrieves all the recipes from JSON file and returns
+        /// it as an IEnumerable collection
+        /// </summary>
         public void OnGet()
         {
             Recipes = RecipeService.GetRecipes();
