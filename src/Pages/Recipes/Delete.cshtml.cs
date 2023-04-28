01a9fe9d94f158af5b0cc82c2ed575e9a1c658e9
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-04-28 13:40:46 -0700

    Added Delete.cshtml and Delete.cshtml.cs to project directory

---
+++ src/Pages/Recipes/Delete.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.Recipes
+{
+	public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
