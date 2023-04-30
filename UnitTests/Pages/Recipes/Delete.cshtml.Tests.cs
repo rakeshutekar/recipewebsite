6a7ab1520a679d53bad36a66851eae228a970fa1
conhodges-su <chodges@seattleu.edu>
2023-04-30 13:14:21 -0700

    Added Delete.cshtml.Tests.cs to the UnitTest directory

---
+++ UnitTests/Pages/Recipes/Delete.cshtml.Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages.Recipes;
+using System;
+using ContosoCrafts.WebSite.Services;
+using System.Linq;
+
+namespace UnitTests.Pages.Recipes
+{
+    public class DeleteTests
+    {
+        
+    }
+}
