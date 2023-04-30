9cca28b573026afdd83744c0457f1c5d5abfded1
torbenwb <torbenwbernhard@gmail.com>
2023-04-30 15:11:42 -0700

    Created Read.cshtml.Tests.cs file
    Created this version of the file to fit directory format. Copied code from ReadTests.cs

---
+++ UnitTests/Pages/Recipes/Read.cshtml.Tests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages.Recipes;
+using System;
+using ContosoCrafts.WebSite.Services;
+using System.Linq;
+
+namespace UnitTests.Pages.Recipes
+{
+    public class ReadTests
+    {
+        #region TestSetup
+        public static ReadModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ReadModel(TestHelper.RecipeService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        /// <summary>
+        /// This method is a unit test that checks the behavior of a web page model's "OnGet" method
+        /// when provided with a valid recipe ID.
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Recipe_ID_Should_Get_And_Set_ReadModel_Recipe()
+        {
+            // Before Calling OnGet ReadModel Recipe property should be null
+            var result = pageModel.Recipe == null;
+            Assert.AreEqual(true, result);
+
+            // Get a valid recipe - first in list
+            var dat = TestHelper.RecipeService.GetRecipes().First();
+            // Call on Get with valid recipe ID
+            pageModel.OnGet(dat.RecipeID);
+
+            // After calling OnGet ReadModel.Recipe property should not be null
+            result = pageModel.Recipe != null;
+            Assert.AreEqual(true, result);
+        }
+        #endregion OnGet
+    }
+}
+
