3381f184f1455ef62f7ec7550ba77550be94793e
Rakesh Utekar <48244158+rakeshutekar1@users.noreply.github.com>
2023-05-02 01:32:57 -0700

    Changes in Create.cshtml.cs
    I changed the Create.cshtml.cs file so that it saves the recipes in JsonRecipeService.cs

--- src/Pages/Recipes/Create.cshtml.cs
+++ src/Pages/Recipes/Create.cshtml.cs
@@ -1,39 +1,42 @@
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-    // CreateModel class represents the page model for the recipe creation page
     public class CreateModel : PageModel
     {
-        // Recipe property represents the model for the recipe data that will be submitted
-        [BindProperty]
-        public Recipe Recipe { get; set; }
+        private readonly ILogger<CreateModel> _logger;
+        private readonly JsonFileRecipeService _recipeService;
 
-        // OnGet method handles GET requests for the recipe creation page
+        public CreateModel(ILogger<CreateModel> logger, JsonFileRecipeService recipeService)
+        {
+            _logger = logger;
+            _recipeService = recipeService;
+        }
+
+        [BindProperty]
+        public RecipeModel Recipe { get; set; }
+
         public void OnGet()
         {
         }
 
-        // OnPostAsync method handles POST requests for the recipe creation page
-       public async Task<IActionResult> OnPostAsync()
+        public IActionResult OnPost()
         {
-            await Task.Yield();
-            // Save the recipe to the database or perform other actions here
-            // For demonstration purposes, we'll just return to the home page
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _recipeService.AddRecipe(Recipe);
+
             return RedirectToPage("/Index");
         }
-
-    }
-
-    // Recipe class represents the model for the recipe data that will be submitted
-    public class Recipe
-    {
-        public string ImgLink { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Ingredients { get; set; }
-        public string Directions { get; set; }
     }
 }
