ddbb8c6375ebccf82c6009bc7c487f51cd55bbb9
Rakesh Utekar <48244158+rakeshutekar1@users.noreply.github.com>
2023-05-02 00:50:56 -0700

    Added a Create.cshtml.Tests.cs file
    created a Added a Create.cshtml.Tests.cs in Unittest so we can do UnitTest

---
+++ UnitTests/Pages/Recipes/Create.cshtml.Tests.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages.Recipes;
+using System;
+using ContosoCrafts.WebSite.Services;
+using System.Linq;
+
+namespace UnitTests.Pages.Recipes
+{
+public class CreateTests
+{
+#region TestSetup
+// Page model for testing
+public static CreateModel pageModel;
+    /// <summary>
+    /// Initialize test environment for Create Tests
+    /// </summary>
+    [SetUp]
+    public void TestInitialize()
+    {
+        pageModel = new CreateModel(TestHelper.RecipeService)
+        {
+            Recipe = new Recipe()
+            {
+                ImgLink = "https://www.example.com/image.jpg",
+                Title = "Test Recipe",
+                Description = "This is a test recipe.",
+                Ingredients = "Ingredient 1, Ingredient 2",
+                Directions = "Step 1, Step 2"
+            }
+        };
+    }
+
+    #endregion TestSetup
+
+    #region OnPostAsync
+    /// <summary>
+    /// This method is a unit test that checks the behavior of a web page model's "OnPostAsync" method
+    /// when provided with valid recipe data.
+    /// </summary>
+    [Test]
+    public async Task OnPostAsync_Valid_Recipe_Data_Should_Save_To_Database_And_Redirect_To_Index()
+    {
+        // Arrange
+        var recipeCountBefore = TestHelper.RecipeService.GetRecipes().Count();
+
+        // Act
+        var result = await pageModel.OnPostAsync();
+
+        // Assert
+        var recipeCountAfter = TestHelper.RecipeService.GetRecipes().Count();
+        Assert.AreEqual(recipeCountBefore + 1, recipeCountAfter);
+        Assert.IsInstanceOf(typeof(RedirectToPageResult), result);
+        var redirectResult = (RedirectToPageResult)result;
+        Assert.AreEqual("/Index", redirectResult.PageName);
+    }
+    #endregion OnPostAsync
+}
+}
