f0eaca1fe4c1ba2de21a3f3308e0df83166cb849
erinjinz <zjin1@seattleu.edu>
2023-05-02 17:30:33 -0700

    Added contents to Update.cshtml.cs
    Update contents similar to contoso crafts update

--- src/Pages/Recipes/Update.cshtml.cs
+++ src/Pages/Recipes/Update.cshtml.cs
@@ -2,15 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages.Recipes
 {
+    /// <summary>
+    /// Mange the Update of the data for a single recipe
+    /// </summary>
+    
 	public class UpdateModel : PageModel
     {
-        public void OnGet()
+        // Data middletier
+        public JsonFileRecipeService RecipeService { get; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="recipeService"></param>
+        public UpdateModel(JsonFileRecipeService recipeService)
         {
+            RecipeService = recipeService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public RecipeModel Recipe { get; set; }
+
+        /// <summary>
+        /// REST Get request
+        /// Loads the data
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Recipe = RecipeService.GetRecipes().FirstOrDefault(r => r.RecipeID.Equals(id));
+        }
+
+        /// <summary>
+        /// Post the model back to the page
+        /// The model is in the class variable Recipe
+        /// Call the data layer to update the data
+        /// Then return to the index page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            RecipeService.UpdateRecipe(Recipe);
+
+            return RedirectToPage("./Index");  
         }
     }
 }
