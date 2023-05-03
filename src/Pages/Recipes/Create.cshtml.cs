120092901cbb5b7b52f859b62ec0db889fa3d460
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-05-03 16:35:21 -0700

    Added comments to Create.cshtml.cs

--- src/Pages/Recipes/Create.cshtml.cs
+++ src/Pages/Recipes/Create.cshtml.cs
@@ -10,20 +10,38 @@ using System;
 
 namespace ContosoCrafts.WebSite.Pages.Recipes
 {
+    /// <summary>
+    /// This page manages the creation of new recipes to the JSON file
+    /// </summary>
     public class CreateModel : PageModel
     {
+        // Logger service injected into the model
         private readonly ILogger<CreateModel> _logger;
+        // Service used to retrieve and store information to the JSON file
         private readonly JsonFileRecipeService _recipeService;
 
+        /// <summary>
+        /// Constructor of the CreateModel takes in the logger and recipe
+        /// service via DI.
+        /// </summary>
+        /// <param name="logger">Logging service</param>
+        /// <param name="recipeService">Recipe service to access JSON</param>
         public CreateModel(ILogger<CreateModel> logger, JsonFileRecipeService recipeService)
         {
             _logger = logger;
             _recipeService = recipeService;
         }
 
+        // Recipe model bound to the page to retrive user input
         [BindProperty]
         public RecipeModel Recipe { get; set; }
 
+        /// <summary>
+        /// Collects information from the form into a Recipe Model, and retrives
+        /// the next recipeID to add it to the file. Adds recipe to the file and
+        /// then redirects to the home page
+        /// </summary>
+        /// <returns></returns>
         public IActionResult OnPost()
         {
             Recipe.RecipeID = _recipeService.NextRecipeID();   
