c7c8fc4ab512af90adb519a522edfa2ee52c729b
Rakesh Utekar <48244158+rakeshutekar1@users.noreply.github.com>
2023-05-02 22:20:38 -0700

    Updated Create.cshtml.Tests.cs
    I have updated the code in Create.cshtml.Tests.cs because I was facing build error

--- UnitTests/Pages/Recipes/Create.cshtml.Tests.cs
+++ UnitTests/Pages/Recipes/Create.cshtml.Tests.cs
@@ -1,59 +1,68 @@
 using NUnit.Framework;
-using ContosoCrafts.WebSite.Pages.Recipes;
-using ContosoCrafts.WebSite.Pages; // Add this line
+using ContosoCrafts.WebSite.Pages;
 using System;
 using ContosoCrafts.WebSite.Services;
 using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 
 namespace UnitTests.Pages.Recipes
 {
-public class CreateTests
-{
-#region TestSetup
-// Page model for testing
-public static CreateModel pageModel;
-    /// <summary>
-    /// Initialize test environment for Create Tests
-    /// </summary>
-    [SetUp]
-    public void TestInitialize()
+    public class CreateTests
     {
-        pageModel = new CreateModel(TestHelper.RecipeService)
+        #region TestSetup
+        // Page model for testing
+        public static CreateModel pageModel;
+
+        /// <summary>
+        /// Initialize test environment for Create Tests
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
         {
-            Recipe = new Recipe()
+            ILogger<CreateModel> logger = new NullLogger<CreateModel>();
+            pageModel = new CreateModel(logger, TestHelper.RecipeService)
             {
-                ImgLink = "https://www.example.com/image.jpg",
-                Title = "Test Recipe",
-                Description = "This is a test recipe.",
-                Ingredients = "Ingredient 1, Ingredient 2",
-                Directions = "Step 1, Step 2"
-            }
-        };
+                Recipe = new RecipeModel()
+                {
+                    Image = "https://www.example.com/image.jpg",
+                    Title = "Test Recipe",
+                    Description = "This is a test recipe.",
+                    Ingredients = new string[] { "Ingredient 1", "Ingredient 2" },
+                    Instructions = new string[] { "Step 1", "Step 2" }
+                }
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnPost
+        /// <summary>
+        /// This method is a unit test that checks the behavior of a web page model's "OnPost" method
+        /// when provided with valid recipe data.
+        /// </summary>
+        [Test]
+        public async Task OnPost_Valid_Recipe_Data_Should_Save_To_Database_And_Redirect_To_Index()
+        {
+            // Arrange
+            var recipeCountBefore = TestHelper.RecipeService.GetRecipes().Count();
+
+            // Act
+            var result = await Task.FromResult(pageModel.OnPost());
+
+            // Assert
+            var recipeCountAfter = TestHelper.RecipeService.GetRecipes().Count();
+            Assert.AreEqual(recipeCountBefore + 1, recipeCountAfter);
+            Assert.IsInstanceOf(typeof(RedirectToPageResult), result);
+            var redirectResult = (RedirectToPageResult)result;
+            Assert.AreEqual("/Index", redirectResult.PageName);
+
+            return;
+        }
+        #endregion OnPost
     }
-
-    #endregion TestSetup
-
-    #region OnPostAsync
-    /// <summary>
-    /// This method is a unit test that checks the behavior of a web page model's "OnPostAsync" method
-    /// when provided with valid recipe data.
-    /// </summary>
-    [Test]
-    public async Task OnPostAsync_Valid_Recipe_Data_Should_Save_To_Database_And_Redirect_To_Index()
-    {
-        // Arrange
-        var recipeCountBefore = TestHelper.RecipeService.GetRecipes().Count();
-
-        // Act
-        var result = await pageModel.OnPostAsync();
-
-        // Assert
-        var recipeCountAfter = TestHelper.RecipeService.GetRecipes().Count();
-        Assert.AreEqual(recipeCountBefore + 1, recipeCountAfter);
-        Assert.IsInstanceOf(typeof(RedirectToPageResult), result);
-        var redirectResult = (RedirectToPageResult)result;
-        Assert.AreEqual("/Index", redirectResult.PageName);
-    }
-    #endregion OnPostAsync
-}
 }
