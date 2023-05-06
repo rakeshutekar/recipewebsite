b08467215ac58b407af0032c7824832e43e23de5
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-05-06 15:30:59 -0700

    Moved js functions from Create.cshtml to site.js

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -2,3 +2,127 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+// Reference for grabbing count of querySelectorAll result
+// https://stackoverflow.com/questions/6991494/javascript-getelementbyid-based-on-a-partial-string
+
+// Variables used across all input forms
+const textType = "text";
+const classType = "form-control";
+const inptGrp = "input-group";
+
+// Function prepared to listen to on-click events of the "+" button under the instructions
+// form group and add new instruction text areas for users to enter in their information
+$("#addBtnInstr").click(function () {
+    var instrs = $('[id ^= "instr"]');
+    var count = instrs.length;
+
+    // prepare html grouping to insert
+    var nextInput = document.createElement("input");
+    var nameAttr = "Recipe.Instructions[" + count + "]";
+    var idAttr = "instr" + count;
+    var placeholderAttr = "Step #" + (count + 1);
+    nextInput.setAttribute("id", idAttr);
+    nextInput.setAttribute("class", classType);
+    nextInput.setAttribute("required", "");
+    nextInput.setAttribute("type", textType);
+    nextInput.setAttribute("name", nameAttr);
+    nextInput.setAttribute("placeholder", placeholderAttr);
+
+
+    var rmvBtn = $("<button></button>");
+    var rmvBtnClass = "btnRmv";
+    rmvBtn.attr("class", "btn btn-sm btn-outline-danger").attr("type", "button").addClass(rmvBtnClass);
+    rmvBtn.html("Remove");
+
+    // set input group innerHTML value to the button created
+    var grpAppend = $("<div></div>");
+    grpAppend.attr("class", "input-group-append");
+    grpAppend.append(rmvBtn);
+
+    var inputDiv = $("<div></div>");
+    var grpClass = "inputGrpInstr";
+    inputDiv.addClass(grpClass).addClass(inptGrp);
+    inputDiv.append(nextInput);
+    inputDiv.append(grpAppend);
+
+    // Insert inside the wrapper
+    $("#wrapperInstr").append(inputDiv);
+
+});
+
+// Delete the specific row of using the text area's delete button
+$("#wrapperInstr").on("click", ".btnRmv", function (e) {
+    e.preventDefault();
+    $(this).parent("div").parent("div").remove();
+
+    // Get the count of the number of rows and re-id them
+    var instrs = $(".inputGrpInstr");
+    var instrCount = instrs.length;
+    var counter = 0;
+
+    // Loop over jquery object set and update the proper model names
+    instrs.each(function () {
+        var name = "Recipe.Instructions[" + counter + "]";
+        $(this).children("input").attr("name", name);
+        counter++;
+    });
+});
+
+
+// Function prepared to listen to on-click events of the "+" button under the ingredients
+// form group and add new ingredients text areas for users to enter in their information
+$("#addBtnIngr").click(function () {
+    var instrs = $('[id ^= "ingr"]');
+    var count = instrs.length;
+
+    // prepare html grouping to insert
+    var nextInput = document.createElement("input");
+    var nameAttr = "Recipe.Ingredients[" + count + "]";
+    var idAttr = "ingr" + count;
+    var placeholderAttr = "Ingredient #" + (count + 1);
+    nextInput.setAttribute("id", idAttr);
+    nextInput.setAttribute("class", classType);
+    nextInput.setAttribute("required", "");
+    nextInput.setAttribute("type", textType);
+    nextInput.setAttribute("name", nameAttr);
+    nextInput.setAttribute("placeholder", placeholderAttr);
+
+
+    var rmvBtn = $("<button></button>");
+    var rmvBtnClass = "btnRmv";
+    rmvBtn.attr("class", "btn btn-sm btn-outline-danger").attr("type", "button").addClass(rmvBtnClass);
+    rmvBtn.html("Remove");
+
+    // set input group innerHTML value to the button created
+    var grpAppend = $("<div></div>");
+    grpAppend.attr("class", "input-group-append");
+    grpAppend.append(rmvBtn);
+
+    var inputDiv = $("<div></div>");
+    var grpClass = "inputGrpIngr";
+    inputDiv.addClass(grpClass).addClass(inptGrp);
+    inputDiv.append(nextInput);
+    inputDiv.append(grpAppend);
+
+    // Insert inside the wrapper
+    $("#wrapperIngr").append(inputDiv);
+
+});
+
+// Delete the specific row of using the text area's delete button
+$("#wrapperIngr").on("click", ".btnRmv", function (e) {
+    e.preventDefault();
+    $(this).parent("div").parent("div").remove();
+
+    // Get the count of the number of rows and re-id them
+    var instrs = $(".inputGrpIngr");
+    var instrCount = instrs.length;
+    var counter = 0;
+
+    // Loop over jquery object set and update the proper model names
+    instrs.each(function () {
+        var name = "Recipe.Ingredients[" + counter + "]";
+        $(this).children("input").attr("name", name);
+        counter++;
+    });
+});
