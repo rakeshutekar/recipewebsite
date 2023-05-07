20487577943694ef1ed6490a127dae19cf9473a6
torbenwb <torbenwbernhard@gmail.com>
2023-05-07 12:05:53 -0700

    Added comments / documentation to RecipesAPIControllerTests.cs

--- UnitTests/Controllers/RecipesAPIControllerTests.cs
+++ UnitTests/Controllers/RecipesAPIControllerTests.cs
@@ -9,20 +9,38 @@ using System.Threading.Tasks;
 
 namespace UnitTests.Controllers
 {
+    /// <summary>
+    /// This class contains unit tests for the RecipesAPIController.
+    /// </summary>
     public class RecipesAPIControllerTests
     {
+        /// <summary>
+        /// This method is executed before each test method is executed.
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
-
+            // Test initialization code goes here
         }
 
+        /// <summary>
+        /// This method tests that the constructor of the RecipesAPIController sets the RecipeService property correctly.
+        /// It creates a new RecipesAPIController object with the RecipeService property set to TestHelper.RecipeService,
+        /// and then verifies that the RecipeService property of the created object is the same as TestHelper.RecipeService.
+        /// </summary>
         [Test]
         public void RecipesAPIController_Constructor_Should_Set_RecipeService()
         {
             RecipesAPIController aPIController = new RecipesAPIController(TestHelper.RecipeService);
             Assert.AreSame(TestHelper.RecipeService, aPIController.RecipeService);
         }
+
+        /// <summary>
+        /// This method tests that the Get method of the RecipesAPIController returns a valid list of RecipeModels.
+        /// It creates a new RecipesAPIController object with the RecipeService property set to TestHelper.RecipeService,
+        /// calls the Get method of the created object, and then verifies that the number of returned RecipeModels
+        /// is the same as the number of recipes returned by TestHelper.RecipeService.GetRecipes().
+        /// </summary>
         [Test]
         public void RecipesAPIController_Get_Should_Return_Valid_RecipeModels()
         {
@@ -31,4 +49,5 @@ namespace UnitTests.Controllers
             Assert.AreEqual(TestHelper.RecipeService.GetRecipes().Count(), models.Count());
         }
     }
+
 }
