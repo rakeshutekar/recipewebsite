e5f08c780e79b4cb3760b4549ccf4e05b8107ab4
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-05-12 16:29:24 -0700

    Added public fields to CommentModel.cs

--- src/Models/CommentModel.cs
+++ src/Models/CommentModel.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 namespace ContosoCrafts.WebSite.Models
 {
+	/// <summary>
+	/// Comments submitted by users are captured in a model associated with a
+	/// single recipe
+	/// </summary>
 	public class CommentModel
 	{
-		public CommentModel()
-		{
-		}
+		// Unique id of the comment
+		public string Id { get; set; } = System.Guid.NewGuid().ToString();
+
+		// First Name of the user submitting the comment
+		public string FirstName { get; set; }
+
+		// Last name of the user submitting the comment
+		public string LastName { get; set; }
+
+		// Comment submitted by the user
+		public string Comment { get; set; }
 	}
 }
-
