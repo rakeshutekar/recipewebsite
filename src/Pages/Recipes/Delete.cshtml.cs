0d9f5f4f6290524e2609d27d0b52a2b592914acc
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-05-12 14:02:43 -0700

    Modified the OnPost() method to revalidate ModelState to fix bug in Delete.cshtml.cs
    Fixes bug where because ModelState was invalid, recipes were not being deleted.

--- src/Pages/Recipes/Delete.cshtml.cs
+++ src/Pages/Recipes/Delete.cshtml.cs
@@ -63,7 +63,17 @@ namespace ContosoCrafts.WebSite.Pages.Recipes
         /// <returns></returns>
         public IActionResult OnPost()
         {
-            if (!ModelState.IsValid)
+            // Model needs to be revalidated as the delete page has minimal
+            // fields filled out and the OnPost() action is stateless so doesn't
+            // carry field information over for non-bound fields.
+            // https://learn.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-6.0
+            var id = Recipe.RecipeID;
+            Recipe = RecipeService.GetRecipe(id);
+
+
+            // Revalidate, then load /index or reload page
+            ModelState.ClearValidationState(nameof(Recipe));
+            if (!TryValidateModel(Recipe, nameof(Recipe)))
             {
                 return Page();
             }
