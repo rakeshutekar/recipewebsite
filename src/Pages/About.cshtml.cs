7633b449f7002eb287100e28351952f2862ed330
Connor Hodges <chodges@seattleu.edu>
2023-05-13 15:46:18 -0700

    Refactored the class name to be AboutModel instead of just about in About.cshtml.cs

--- src/Pages/About.cshtml.cs
+++ src/Pages/About.cshtml.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
@@ -6,15 +11,15 @@ namespace ContosoCrafts.WebSite.Pages
     /// <summary>
     /// Represents a model for the About page that provides information about the application.
     /// </summary>
-    public class About : PageModel
+    public class AboutModel : PageModel
     {
-        private readonly ILogger<About> _logger;
+        private readonly ILogger<AboutModel> _logger;
 
         /// <summary>
         /// Initializes a new instance of the About class with the specified logger.
         /// </summary>
         /// <param name="logger">The logger to use for logging information about the application.</param>
-        public About(ILogger<About> logger)
+        public AboutModel(ILogger<AboutModel> logger)
         {
             _logger = logger;
         }
@@ -22,8 +27,9 @@ namespace ContosoCrafts.WebSite.Pages
         /// <summary>
         /// Handles HTTP GET requests for the About page.
         /// </summary>
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            return Page();
         }
     }
 }
