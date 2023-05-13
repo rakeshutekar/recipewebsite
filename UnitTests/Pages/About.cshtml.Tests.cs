416621af10d239b997dcb4787b791b492459282a
Connor Hodges <chodges@seattleu.edu>
2023-05-13 15:50:16 -0700

    Added tests for coverage to About.cshtml.Tests.cs

--- UnitTests/Pages/About.cshtml.Tests.cs
+++ UnitTests/Pages/About.cshtml.Tests.cs
@@ -14,6 +14,31 @@ namespace UnitTests.Pages
 {
     public class AboutTests
     {
-        
+        #region TestSetup
+        public static AboutModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<AboutModel>>();
+            pageModel = new AboutModel(MockLoggerDirect)
+            {
+            };
+        }
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Should_Return_Page_Result_And_Valid_Model()
+        {
+            // Arrange
+
+            // Act
+            var validModel = pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnGet
     }
 }
