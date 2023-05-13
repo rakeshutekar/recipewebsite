e0fe1dd136c3badf38a502764832abf63b97031c
Connor Hodges <chodges@seattleu.edu>
2023-05-13 15:49:38 -0700

    Added new unit test class About.cshtml.Tests.cs

---
+++ UnitTests/Pages/About.cshtml.Tests.cs
@@ -0,0 +1,19 @@
+﻿using ContosoCrafts.WebSite.Controllers;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Pages;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages
+{
+    public class AboutTests
+    {
+        
+    }
+}
