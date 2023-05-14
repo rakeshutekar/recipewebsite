8fb76cce67d008820186013b1fc2774c6b6bc0a5
torbenwb <torbenwbernhard@gmail.com>
2023-05-14 12:27:12 -0700

    Created Index.cshtml.Tests.cs to test main Index page

---
+++ UnitTests/Pages/Index.cshtml.Tests.cs
@@ -0,0 +1,40 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages
+{
+    public class IndexTests
+    {
+        #region TestSetup
+        public static IndexModel pageModel; // mock page model used for testing
+
+        /// <summary>
+        /// Test Initialize - performs initialization before running any tests
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+            pageModel = new IndexModel(MockLoggerDirect,TestHelper.RecipeService)
+            {
+            };
+        }
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Should_Set_Recipes_To_All_Recipes_Retrieved_From_Recipe_Service()
+        {
+
+        }
+        #endregion OnGet
+    }
+}
