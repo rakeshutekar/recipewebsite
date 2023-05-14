cc60f8fa1e2ba86a0a4687f40bde679add602502
torbenwb <torbenwbernhard@gmail.com>
2023-05-14 12:18:50 -0700

    Added comments and achieved 100% code coverage in Error.cshtml.Tests.cs

--- UnitTests/Pages/Error.cshtml.Tests.cs
+++ UnitTests/Pages/Error.cshtml.Tests.cs
@@ -16,8 +16,11 @@ namespace UnitTests.Pages
     public class ErrorTests
     {
         #region TestSetup
-        public static ErrorModel pageModel;
+        public static ErrorModel pageModel; // mock page model used for testing
 
+        /// <summary>
+        /// Test Initialize - performs initialization before running any tests
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -29,13 +32,15 @@ namespace UnitTests.Pages
         #endregion TestSetup
 
         #region RequestID
+        /// <summary>
+        /// Test to ensure that the ShowRequestId property returns expected result
+        /// </summary>
         [Test]
         public void ShowRequestId_Should_Return_False_When_RequestId_Is_Null_Or_Empty()
         {
             // Arrange
             ErrorModel tempModel = new ErrorModel(null);
             
-
             // Act
 
             // Assert
@@ -44,6 +49,10 @@ namespace UnitTests.Pages
         #endregion
 
         #region OnGet
+        /// <summary>
+        /// Test to ensure that OnGet sets the RequestId field to the Current Activity ID if the
+        /// Current Activity is not null.
+        /// </summary>
         [Test]
         public void OnGet_Should_Set_RequestId_To_Activity_Current_Id_If_Activity_Current_Is_Not_Null()
         {
@@ -55,15 +64,17 @@ namespace UnitTests.Pages
             // Assert
             Assert.AreEqual(pageModel.RequestId, Activity.Current.Id);
         }
+        /// <summary>
+        /// Test to ensure that OnGet sets the RequestId field to the HttpContext Trace Identifier if
+        /// the current activity is null.
+        /// </summary>
         [Test]
         public void OnGet_Should_Set_RequestId_To_HttpContext_TraceIdentifier_If_Current_Activity_Is_Null()
         {
-            Console.WriteLine(TestHelper.HttpContextDefault.TraceIdentifier);
             // Arrange
             if (Activity.Current != null) Activity.Current.Stop();
 
             // Act
-            Console.WriteLine(pageModel.HttpContext == null);
             pageModel.PageContext = TestHelper.PageContext;
             pageModel.OnGet();
             // Assert
