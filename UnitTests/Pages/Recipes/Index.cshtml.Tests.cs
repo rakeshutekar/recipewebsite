4b3f924e268568ed87f676654c6b5066ae40737e
Connor Hodges <chodges@seattleu.edu>
2023-05-15 17:23:47 -0700

    Modified OnGet() tests to bring coverage back up to 100% in Index.cshtml.Tests

--- UnitTests/Pages/Recipes/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Recipes/Index.cshtml.Tests.cs
@@ -1,6 +1,7 @@
 ﻿using ContosoCrafts.WebSite.Controllers;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Pages.Recipes;
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -13,20 +14,22 @@ namespace UnitTests.Pages.Recipes
     /// <summary>
     /// IndexTests includes unit tests for the IndexList page mode.
     /// </summary>
-    internal class IndexListTests
+    internal class IndexModelTests
     {
+        #region TestSetup
         [SetUp]
         /// <summary>
         /// Initializes test environment
         /// </summary>
         public void TestInitialize()
         {
-            pageModel = new IndexList(null, TestHelper.RecipeService)
+            pageModel = new IndexModel(null, TestHelper.RecipeService)
             {
             };
         }
         // A static page from the IndexList model class
-        public static IndexList pageModel;
+        public static IndexModel pageModel;
+
 
         /// <summary>
         /// Tests that constructor correctly injects recipes service 
@@ -38,7 +41,9 @@ namespace UnitTests.Pages.Recipes
             var result = pageModel.RecipeService != null;
             Assert.AreEqual(true, result);
         }
+        #endregion TestSetup
 
+        #region OnGet
         /// <summary>
         /// Tests that OnGet correctly retrieves recipes from recipe service
         /// Asserts that pageMode.Recipes has equal count to RecipeService.Recipes
@@ -62,5 +67,39 @@ namespace UnitTests.Pages.Recipes
                 Assert.AreEqual(true, pageModelRecipes[i].ToString() == recipeServiceRecipes[i].ToString());    
             }
         }
+
+        /// <summary>
+        /// This test ensures that invalid filter queries are redirected to the error page
+        /// </summary>
+        [Test]
+        public void OnGet_Invalid_Filter_Query_Redirects_To_Error_Page()
+        {
+            // Arrange
+
+            // Act
+            pageModel.Filter = "test";
+            var pageResult = pageModel.OnGet() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageResult.PageName.Contains("Error"));
+        }
+
+        /// <summary>
+        /// This test ensures that when the filter = "cuisines" it returns a valid set
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Filter_Query_Retrieves_Cuisines_And_Valid_Model()
+        {
+            // Arrange
+            const string CUISINE_FILTER = "cuisines";
+
+            // Act
+            pageModel.Filter = CUISINE_FILTER;
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
     }
+        #endregion OnGet
 }
