5ec359a74d3d64332c828609d31ed617a634c893
torbenwb <torbenwbernhard@gmail.com>
2023-05-17 14:48:53 -0700

    Removed unnecessary using statements from About.cshtml.Tests.cs

--- UnitTests/Pages/About.cshtml.Tests.cs
+++ UnitTests/Pages/About.cshtml.Tests.cs
@@ -1,14 +1,7 @@
-﻿using ContosoCrafts.WebSite.Controllers;
-using ContosoCrafts.WebSite.Models;
-using ContosoCrafts.WebSite.Pages;
+﻿using ContosoCrafts.WebSite.Pages;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UnitTests.Pages
 {
