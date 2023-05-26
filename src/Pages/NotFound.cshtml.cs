833a7e060355ba9c88f40d762359e035a91144b2
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-05-26 11:35:50 -0700

    Updated using statements in NotFound.cshtml.cs

--- src/Pages/NotFound.cshtml.cs
+++ src/Pages/NotFound.cshtml.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
