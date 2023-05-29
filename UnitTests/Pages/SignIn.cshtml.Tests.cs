3bf21f3748cda0ce70ba72430b21bdc23f510b7f
Connor Hodges <chodges@seattleu.edu>
2023-05-29 10:19:13 -0700

    Added tests against SignInModel in SignIn.cshtml.Tests.cs

---
+++ UnitTests/Pages/SignIn.cshtml.Tests.cs
@@ -0,0 +1,58 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests.Pages
+{
+    /// <summary>
+    /// Unit test class for testing the SignIn page
+    /// </summary>
+    public class SignInTests
+    {
+        #region TestSetup
+        // mock page model used for testing
+        public static SignInModel pageModel;
+
+        /// <summary>
+        /// Test initialize - performs initialization before running any tests
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<SignInModel>>();
+            pageModel = new SignInModel(MockLoggerDirect);
+        }
+        #endregion TestSetup
+
+        #region OnGet
+        /// <summary>
+        /// OnGet method test to ensure page model is valid on OnGet call
+        /// </summary>
+        [Test]
+        public void OnGet_Should_Return_Page_Result_And_Valid_Model()
+        {
+            // Act
+            var validModel = pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnGet
+
+        #region OnPost
+        /// <summary>
+        /// OnPost method test to ensure page model is valid on OnPost call
+        /// </summary>
+        [Test]
+        public void OnPost_Should_Return_Page_Result_And_Valid_Model()
+        {
+            // Act
+            var validModel = pageModel.OnPost();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnPost
+    }
+}
