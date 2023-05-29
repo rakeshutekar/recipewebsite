8a0c8587639818f197b650cda82028fdc15cda2c
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2023-05-29 10:16:47 -0700

    Updated page to include logger in SignIn.cshtml.cs

--- src/Pages/SignIn.cshtml.cs
+++ src/Pages/SignIn.cshtml.cs
@@ -13,17 +13,30 @@ namespace ContosoCrafts.WebSite.Pages
     /// </summary>
     public class SignInModel : PageModel
     {
+        // Private logger class field
+        private readonly ILogger<SignInModel> _logger;
+
         /// <summary>
-        /// OnGet method prepares the page on pafe load
+        /// Constructor injects logger class into the page
         /// </summary>
-        public void OnGet()
+        /// <param name="logger">Logger service injected via DI</param>
+        public SignInModel(ILogger<SignInModel> logger)
         {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// OnGet method prepares the page on page load
+        /// </summary>
+        public IActionResult OnGet()
+        {
+            return Page();
         }
 
         /// <summary>
         /// OnPost action after user selects the sign-in button
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Returns user to the same page as before</returns>
         public IActionResult OnPost()
         {
             return Page();
