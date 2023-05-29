22aae12fe90f0c213452d5b2ba9ece118156322e
erinjinz <zjin1@seattleu.edu>
2023-05-28 20:09:15 -0700

    Created SignUp.cshtml.cs

---
+++ src/Pages/SignUp.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// SignUpModel represents the page for creating an account
+    /// </summary>
+	public class SignUpModel : PageModel
+    {
+        /// <summary>
+        /// OnGet method prepares the page on page load
+        /// </summary>
+        public void OnGet()
+        {
+        }
+
+        /// <summary>
+        /// OnPost action after user selects the sign-up button
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OnPost()
+        {
+            return Page();
+        }
+    }
+}
