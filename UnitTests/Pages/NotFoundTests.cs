e0e9c8a139d06085c924208cbba966eb832259aa
torbenwb <torbenwbernhard@gmail.com>
2023-05-30 07:45:03 -0700

    Added comments documentation and formatting to NotFoundTests.cs

--- UnitTests/Pages/NotFoundTests.cs
+++ UnitTests/Pages/NotFoundTests.cs
@@ -1,8 +1,5 @@
 ﻿using ContosoCrafts.WebSite.Pages;
-using Microsoft.Extensions.Logging;
-using Moq;
 using NUnit.Framework;
-using System.Linq;
 
 namespace UnitTests.Pages
 {
@@ -11,7 +8,6 @@ namespace UnitTests.Pages
     {
         #region TestSetup
         public static NotFoundModel pageModel; // mock page model used for testing
-
         /// <summary>
         /// Test Initialize - performs initialization before running any tests
         /// </summary>
@@ -22,16 +18,20 @@ namespace UnitTests.Pages
             {
             };
         }
-
+        /// <summary>
+        /// Tests that NotFoundModel.type correctly gets / sets type value
+        /// </summary>
         [Test]
-        public void NotFound_OnGet_Type_Should_Get_Set_Type()
+        public void NotFound_Type_Should_Get_Set_Type()
         {
             // Set page model type using set
             pageModel.type = NotFoundTypes.Recipe;
             // Assert page model type is equal to set value
             Assert.AreEqual(pageModel.type, NotFoundTypes.Recipe);
         }
-
+        /// <summary>
+        /// Tests that response get / set correctly gets and sets response
+        /// </summary>
         [Test]
         public void NotFound_Response_Should_Get_Set_Response()
         {
@@ -41,9 +41,11 @@ namespace UnitTests.Pages
             // assert page model response is equal to set value
             Assert.AreEqual(pageModel.response, response);
         }
-
+        /// <summary>
+        /// Tests that when OnGet is called model response is equal to RECIPE_NOT_FOUND_RESPONSE
+        /// </summary>
         [Test]
-        public void NotFound_OnGet_Type_Recipe_Response_Should_Be_Not_Found()
+        public void NotFound_OnGet_Type_Recipe_Response_Should_Be_Recipe_Not_Found_Response()
         {
             // Set page model response type
             pageModel.type = NotFoundTypes.Recipe;
@@ -52,16 +54,18 @@ namespace UnitTests.Pages
             // Assert - response is equal to not found response
             Assert.AreEqual(pageModel.response, NotFoundModel.RECIPE_NOT_FOUND_RESPONSE);
         }
-
+        /// <summary>
+        /// Tests that when OnGet is called model response is equal to DEFAULT_NOT_FOUND_RESPONSE
+        /// </summary>
         [Test]
-        public void NotFound_OnGet_Type_Default_Response_Should_Be_Default()
+        public void NotFound_OnGet_Type_Default_Response_Should_Be_Default_Not_Found_Response()
         {
             // Arrange - set page model response type to none
             pageModel.type = NotFoundTypes.None;
             // Act - Call OnGet
             pageModel.OnGet();
             // Assert - response is equal to default response
-            Assert.AreEqual(pageModel.response, NotFoundModel.RECIPE_DEFAULT_RESPONSE);
+            Assert.AreEqual(pageModel.response, NotFoundModel.DEFAULT_NOT_FOUND_RESPONSE);
         }
         #endregion TestSetup
     }
