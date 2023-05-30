2a007999d564c7f3a80d3c9bbe9727a059a33c5a
torbenwb <torbenwbernhard@gmail.com>
2023-05-30 07:37:53 -0700

    Added tests to NotFoundTests.cs

--- UnitTests/Pages/NotFoundTests.cs
+++ UnitTests/Pages/NotFoundTests.cs
@@ -22,6 +22,47 @@ namespace UnitTests.Pages
             {
             };
         }
+
+        [Test]
+        public void NotFound_OnGet_Type_Should_Get_Set_Type()
+        {
+            // Set page model type using set
+            pageModel.type = NotFoundTypes.Recipe;
+            // Assert page model type is equal to set value
+            Assert.AreEqual(pageModel.type, NotFoundTypes.Recipe);
+        }
+
+        [Test]
+        public void NotFound_Response_Should_Get_Set_Response()
+        {
+            const string response = "TEST"; // TEST value
+            // Set page model response using set
+            pageModel.response = response;
+            // assert page model response is equal to set value
+            Assert.AreEqual(pageModel.response, response);
+        }
+
+        [Test]
+        public void NotFound_OnGet_Type_Recipe_Response_Should_Be_Not_Found()
+        {
+            // Set page model response type
+            pageModel.type = NotFoundTypes.Recipe;
+            // Act - call OnGet
+            pageModel.OnGet();
+            // Assert - response is equal to not found response
+            Assert.AreEqual(pageModel.response, NotFoundModel.NOT_FOUND_RESPONSE);
+        }
+
+        [Test]
+        public void NotFound_OnGet_Type_Default_Response_Should_Be_Default()
+        {
+            // Arrange - set page model response type to none
+            pageModel.type = NotFoundTypes.None;
+            // Act - Call OnGet
+            pageModel.OnGet();
+            // Assert - response is equal to default response
+            Assert.AreEqual(pageModel.response, NotFoundModel.DEFAULT_RESPONSE);
+        }
         #endregion TestSetup
     }
 }
