11c9a16358a4e5fbbc8ed3bb858f8f66eb0f722d
torbenwb <torbenwbernhard@gmail.com>
2023-05-30 07:26:44 -0700

    Created NotFoundTests.cs

---
+++ UnitTests/Pages/NotFoundTests.cs
@@ -0,0 +1,27 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Linq;
+
+namespace UnitTests.Pages
+{
+
+    public class NotFoundTests
+    {
+        #region TestSetup
+        public static NotFoundModel pageModel; // mock page model used for testing
+
+        /// <summary>
+        /// Test Initialize - performs initialization before running any tests
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new NotFoundModel()
+            {
+            };
+        }
+        #endregion TestSetup
+    }
+}
