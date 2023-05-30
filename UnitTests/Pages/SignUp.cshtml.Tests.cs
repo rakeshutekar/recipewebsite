491a19fd3fdf8f9e7ded0d1726f56fdad7117a05
torbenwb <torbenwbernhard@gmail.com>
2023-05-30 07:55:12 -0700

    Created SignUp.cshtml.Tests.cs

---
+++ UnitTests/Pages/SignUp.cshtml.Tests.cs
@@ -0,0 +1,38 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using NUnit.Framework;
+
+namespace UnitTests.Pages
+{
+    public class SignUpTests
+    {
+        #region TestSetup
+        // mock page model used for testing
+        public static SignUpModel pageModel;
+
+        /// <summary>
+        /// Test initialize - performs initialization before running any tests
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new SignUpModel();
+        }
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void SignUpModel_OnGet_Should_Not_Throw_An_Exception()
+        {
+            Assert.DoesNotThrow(() => pageModel.OnGet());
+        }
+        #endregion OnGet
+
+        #region OnPost
+        [Test]
+        public void SignUpModel_OnPost_Should_Return_Not_Null()
+        {
+            Assert.NotNull(pageModel.OnPost());
+        }
+        #endregion OnPost
+    }
+}
